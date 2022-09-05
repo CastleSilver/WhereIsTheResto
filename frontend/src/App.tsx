3aa36127fb1c45131c8f8bdd773c535cd0385c80
이종현 <dlwhdgus7609@naver.com>
2022-09-05 14:34:57 +0900

    fix : file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>hi</h1>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
     </div>
   );
 }

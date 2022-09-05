633074b73df331c1a57d8b152c6382fe6ef50511
이종현 <dlwhdgus7609@naver.com>
2022-09-05 16:33:19 +0900

    fix : file structure

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     "plugin:import/warnings", // 설치한 경우
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "react-app",
+    "prettier"
   ],
   rules: {
     "linebreak-style": 0,
@@ -22,16 +24,17 @@ module.exports = {
     "react/prop-types": 0,
     "react/jsx-filename-extension": [
       2,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
     ],
     "jsx-a11y/no-noninteractive-element-interactions": 0,
     "@typescript-eslint/explicit-module-boundary-types": 0,
+    "prettier/prettier": ["error", { endOfLine: "auto" }]
   },
   settings: {
     "import/resolver": {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  }
 };
---
+++ frontend/src/redux/User/reducer/reducer.jsx
@@ -0,0 +1,7 @@
+const loginState = {};
+
+function reducer(action, state = loginState) {
+  return { ...state };
+}
+
+export default reducer;

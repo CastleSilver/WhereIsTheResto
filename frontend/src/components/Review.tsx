0f53932efa4aa27a1fee1681af2af0296fc2fb46
원찬호 <chwon03@naver.com>
2022-09-20 14:10:42 +0900

    update/ Front Code Front 코드 업데이트 했습니다.

---
+++ frontend/src/components/Review.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Review() {
+  return (
+    <>
+      <div>Review</div>
+      <p>review.rating</p>
+      <p>review.content</p>
+      <p>review.tags</p>
+    </>
+  )
+}

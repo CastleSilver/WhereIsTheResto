404d8a9b1a8692ff7c44a120705d6218cf729bd1
Hyeon Yeol Kim <gusduf612@gmail.com>
2022-09-21 14:25:03 +0900

    feat : create comment crawling
    유튜브 댓글 크롤링

--- backend-django/youtubekim.py
+++ backend-django/youtubekim.py
@@ -1,9 +1,17 @@
+#-*- coding:utf-8 -*-
+import urllib3
+import json
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from oauth2client.tools import argparser
 from pprint import pprint
 import pandas as pd
 import re
+
+# pip install --upgrade google-api-python-client
+# pip install --upgrade google-auth-oauthlib google-auth-httplib2
+# pip install oauth2client
+# pip install pandas
 
 # https://console.cloud.google.com/apis/credentials 여기서 API발급받아 사용
 DEVELOPER_KEY='AIzaSyA1IvoD6CLeYQZDVWlgkTFLdZjt0OPJQyk'
@@ -18,7 +26,7 @@ playlists = youtube.playlists().list(
     maxResults = 10
 ).execute()
 
-pprint(playlists)
+# pprint(playlists)
 
 playlistitems_list_request = youtube.playlistItems().list(
     playlistId = 'PLCNYoGrzVJuUWTlwZ2CH9nfQF08959pIj',
@@ -38,9 +46,8 @@ while playlistitems_list_request:
     for item in playlistitems_list_response['items']:
         title = item['snippet']['title']
         description = item['snippet']['description']
-        video_id = item['id']
+        video_id = item['snippet']['resourceId']['videoId']
         info = description.split('\n')
-        
         
         video_info_title.append(info[1])
         video_info_location.append(info[2])
@@ -59,5 +66,76 @@ video_df['video_description'] = video_description
 video_df['video_info_title'] = video_info_title
 video_df['video_info_location'] = video_info_location
 
+# 여기까지가 김사원 세끼 서울 지역 노포 정보 불러오기
+# 아래부터는 영상에 대한 댓글 api
 
-print(video_df)
+comments = []
+
+print(video_ids[0])
+
+comment_list_response = youtube.commentThreads().list(
+    videoId = video_ids[1],
+    order = 'relevance',
+    part = 'snippet,replies',
+    maxResults = 100
+).execute()
+
+while comment_list_response:
+    for item in comment_list_response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append([comment['textDisplay'], comment['authorDisplayName'], comment['likeCount']])
+
+    if item['snippet']['totalReplyCount'] > 0:
+        for reply_item in item['replies']['comments']:
+            reply = reply_item['snippet']
+            comments.append([reply['textDisplay'], reply['authorDisplayName'], reply['likeCount']])
+    
+    if 'nextPageToken' in comment_list_response:
+        comment_list_response = youtube.commentThreads().list(
+            videoId = video_ids[1],
+            order = 'relevance',
+            part = 'snippet,replies',
+            pageToken = comment_list_response['nextPageToken'],
+            maxResults = 100
+        ).execute()
+    else:
+        break
+
+comment_df = pd.DataFrame(comments)
+comment_df.columns = ['comment', 'author', 'like']
+
+comment_df.to_csv("comment_list.csv", mode='w')
+
+
+# // 언어 분석 기술 문어/구어 중 한가지만 선택해 사용
+# // 언어 분석 기술(문어)
+openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU" 
+# // 언어 분석 기술(구어)
+openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU_spoken"
+ 
+accessKey = "YOUR_ACCESS_KEY"
+analysisCode = "ANALYSIS_CODE"
+text = ""
+ 
+# // 언어 분석 기술(문어)
+text += 'a'
+
+requestJson = {
+    "access_key": accessKey,
+    "argument": {
+        "text": text,
+        "analysis_code": analysisCode
+    }
+}
+ 
+http = urllib3.PoolManager()
+response = http.request(
+    "POST",
+    openApiURL,
+    headers={"Content-Type": "application/json; charset=UTF-8"},
+    body=json.dumps(requestJson)
+)
+ 
+print("[responseCode] " + str(response.status))
+print("[responBody]")
+print(str(response.data,"utf-8"))

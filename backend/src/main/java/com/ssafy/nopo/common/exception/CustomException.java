038200524310b1a7a0e82a64c1a30d3a3d36343f
허성은 <aysel0230@gmail.com>
2022-09-26 04:18:54 +0900

    feat: add CustomException and make ErrorCode

---
+++ backend/src/main/java/com/ssafy/nopo/common/exception/CustomException.java
@@ -0,0 +1,13 @@
+package com.ssafy.nopo.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
+}

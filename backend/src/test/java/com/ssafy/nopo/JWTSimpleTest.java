4462df359db0f754d2a20671d2e5f4fcc0e49476
minju0919 <minju0919@naver.com>
2022-09-25 16:10:24 +0900

    chore: delete test code

--- backend/src/test/java/com/ssafy/nopo/JWTSimpleTest.java
+++ backend/src/test/java/com/ssafy/nopo/JWTSimpleTest.java
@@ -1,21 +1,12 @@
 package com.ssafy.nopo;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.Base64;
-import java.util.Date;
-
 public class JWTSimpleTest {
 
-    private void printTeoken(String token){
-        String[] tokens = token.split("\\.");
-        System.out.println("header: " + new String(Base64.getDecoder().decode(tokens[0])));
-        System.out.println("body: " + new String(Base64.getDecoder().decode(tokens[1])));
-    }
+//    private void printTeoken(String token){
+//        String[] tokens = token.split("\\.");
+//        System.out.println("header: " + new String(Base64.getDecoder().decode(tokens[0])));
+//        System.out.println("body: " + new String(Base64.getDecoder().decode(tokens[1])));
+//    }
 
 //    @DisplayName("1. jjwt를 이용한 토큰 테스트")
 //    @Test
@@ -46,23 +37,23 @@ public class JWTSimpleTest {
 //        System.out.println(tokenInfo);
 //    }
 
-    @DisplayName("3. 만료 시간 테스트")
-    @Test
-    void test_3() throws InterruptedException {
-        final Algorithm AL = Algorithm.HMAC256("jongwon");
-        String token = JWT.create().withSubject("a1234")
-                .withNotBefore(new Date(System.currentTimeMillis() + 1000))
-                .withExpiresAt(new Date(System.currentTimeMillis() + 3000))
-                .sign(AL);
-
-        try {
-            DecodedJWT verify = JWT.require(AL).build().verify(token);
-            System.out.println(verify.getClaims());
-        } catch(Exception e){
-            System.out.println("유효하지 않은 토큰입니다...");
-            DecodedJWT decode = JWT.decode(token);
-            System.out.println(decode.getClaims());
-        }
-    }
+//    @DisplayName("3. 만료 시간 테스트")
+//    @Test
+//    void test_3() throws InterruptedException {
+//        final Algorithm AL = Algorithm.HMAC256("jongwon");
+//        String token = JWT.create().withSubject("a1234")
+//                .withNotBefore(new Date(System.currentTimeMillis() + 1000))
+//                .withExpiresAt(new Date(System.currentTimeMillis() + 3000))
+//                .sign(AL);
+//
+//        try {
+//            DecodedJWT verify = JWT.require(AL).build().verify(token);
+//            System.out.println(verify.getClaims());
+//        } catch(Exception e){
+//            System.out.println("유효하지 않은 토큰입니다...");
+//            DecodedJWT decode = JWT.decode(token);
+//            System.out.println(decode.getClaims());
+//        }
+//    }
 
 }

b876a04f05e3ecdd071afe8eb8dda2bb53bf5528
HYKdev <gusduf612@gmail.com>
2022-09-26 03:56:20 +0900

    chore : update youtube crawling setting
    유튜브 댓글 크롤링 코드 수정

--- backend-django/youtubekim.py
+++ backend-django/youtubekim.py
@@ -120,37 +120,4 @@ comment_df.columns = ['comment', 'author', 'like']
 comment_df.to_csv("comment_list.csv", mode='w', encoding='utf8')
 
 
-#-*- coding:utf-8 -*-
-import urllib3
-import json
- 
-# // 언어 분석 기술 문어/구어 중 한가지만 선택해 사용
-# // 언어 분석 기술(문어)
-openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU" 
-# // 언어 분석 기술(구어)
-openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU_spoken"
- 
-accessKey = "YOUR_ACCESS_KEY"
-analysisCode = "ANALYSIS_CODE"
-text = "YOUR_SENTENCE"
- 
-requestJson = {
-    "access_key": accessKey,
-    "argument": {
-        "text": text,
-        "analysis_code": analysisCode
-    }
-}
- 
-http = urllib3.PoolManager()
-response = http.request(
-    "POST",
-    openApiURL,
-    headers={"Content-Type": "application/json; charset=UTF-8"},
-    body=json.dumps(requestJson)
-)
- 
-print("[responseCode] " + str(response.status))
-print("[responBody]")
-print(str(response.data,"utf-8"))
 

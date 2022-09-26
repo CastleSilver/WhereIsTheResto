46c53b4bc7090ba0a34f6435fc27294c3e101680
Hyeon Yeol Kim <gusduf612@gmail.com>
2022-09-26 10:08:35 +0900

    feat: create kakaologin front
    프론트 카카오 로그인 제작

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -1,12 +1,12 @@
 // Images
-import mainLogo from "../../assets/main_logo.png"
-import feZero from "../../assets/feZero.jpg"
-import kakaoBtn from "../../assets/kakao_login_btn.png"
+import mainLogo from "../../assets/main_logo.png";
+import feZero from "../../assets/feZero.jpg";
+import kakaoBtn from "../../assets/kakao_login_btn.png";
 
 // MUI
-import Grid from "@mui/material/Grid"
-import Box from "@mui/material/Box"
-import { Link } from "react-router-dom"
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { Link } from "react-router-dom";
 
 export default function StartPage() {
   // 상단 오렌지색 수평선
@@ -14,7 +14,11 @@ export default function StartPage() {
     paddingTop: "5vh",
     marginBottom: "5vh",
     borderBottom: "2px solid rgb(216, 99, 69)",
-  }
+  };
+
+  const REST_API_KEY = "4451e1614fc6653da21821b099437e5a";
+  const REDIRECT_URI = "http://localhost:5173/oauth/kakao/callback";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return (
     <>
@@ -31,10 +35,10 @@ export default function StartPage() {
       >
         <img src={mainLogo} alt="logo" width="90%" />
         <img src={feZero} alt="logo" width="50%" />
-        <Link to="main">
+        <a href={KAKAO_AUTH_URL}>
           <img src={kakaoBtn} alt="logo" width="50%" />
-        </Link>
+        </a>
       </Grid>
     </>
-  )
+  );
 }

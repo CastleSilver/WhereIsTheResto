2adf3836c77a51266a4b3bfa00fbcfa05774f283
원찬호 <chwon03@naver.com>
2022-09-26 09:49:21 +0900

    add/MyLab 컴포넌트 제작을 위해 사용하는 실험 파일들

---
+++ frontend/src/needConfirm/NeedConfirm.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Map1 from "./Map1"
+import Map2 from "./Map2"
+
+export default function NeedConfirm() {
+  return (
+    <>
+      <Map1 />
+      <hr />
+      <Map2 />
+    </>
+  )
+}

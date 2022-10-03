7ad500eb0463776e2a0813fa8d87a716c2691927
원찬호 <chwon03@naver.com>
2022-10-03 16:21:35 +0900

    update/startpage StartPage와 AZTI Page 리뉴얼

--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -2,64 +2,71 @@
 import mainLogo from "../../assets/Logo.png"
 import feZero from "../../assets/feZero.jpg"
 import kakaoBtn from "../../assets/kakao_login_btn.png"
-import { StartImg } from "../../assets/imageUrl"
 
 // MUI
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
-import { Link, useLocation } from "react-router-dom"
-import { useEffect, useRef } from "react"
-
-const pageStyle = {
-  height: "95vh",
-  display: "flex",
-}
-
-const logoStyle = {
-  marginBottom: "36px",
-  width: "85%",
-}
-
-const imgStyle = {
-  marginBottom: "36px",
-  width: "85%",
-}
-
-const kakaoStyle = {
-  width: "85%",
-}
+import { Link } from "react-router-dom"
 
 export default function StartPage() {
   // 상단 오렌지색 수평선
-  const linkRef = useRef()
+  const topAreaStyle = {
+    paddingTop: "5vh",
+    marginBottom: "5vh",
+    borderBottom: "2px solid rgb(216, 99, 69)",
+  }
 
   const REST_API_KEY = "4451e1614fc6653da21821b099437e5a"
   const REDIRECT_URI = "http://localhost:5173/oauth/kakao/callback"
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
+  const koreanAztiType = {
+    mcis: "감성 알뜰 인싸 주당",
+    dcis: "현실 알뜰 인싸 주당",
+    mnis: "감성 호탕 인싸 주당",
+    dnis: "현실 호탕 인싸 주당",
+    mchs: "감성 알뜰 힙스터 주당",
+    dchs: "현실 알뜰 힙스터 주당",
+    mnhs: "감성 호탕 힙스터 주당",
+    dnhs: "현실 호탕 힙스터 주당",
+    mchc: "감성 알뜰 힙스터 술린이",
+    dchc: "현실 알뜰 힙스터 술린이",
+    mnhc: "감성 호탕 힙스터 술린이",
+    dnhc: "현실 호탕 힙스터 술린이",
+    mcic: "감성 알뜰 인싸 술린이",
+    dcic: "현실 알뜰 인싸 술린이",
+    mnic: "감성 호탕 인싸 술린이",
+    dnic: "현실 호탕 인싸 술린이",
+  }
+  let sPick = Math.floor(Math.random() * 16)
   return (
-    <Box sx={pageStyle}>
+    <>
+      {/* 상단 오렌지색 수평선 */}
+      <Box sx={topAreaStyle}></Box>
+
       {/* 로고, 캐릭터, 카카오 로그인 영역 */}
       <Grid
         container
+        display="flex"
         direction="column"
         alignItems="center"
         justifyContent="center"
       >
-        <Grid item sx={logoStyle}>
-          <img src={mainLogo} alt="logo" width="100%" />
-        </Grid>
-        <Grid item sx={imgStyle}>
-          <StartImg />
-        </Grid>
-        <Grid item sx={kakaoStyle}>
-          <a href={KAKAO_AUTH_URL}>
-            <img src={kakaoBtn} alt="logo" width="100%" />
-            {/* 이 부분 떄문에 에러가 발생하는데, 배포 하면 지울 부분이라 괜찮을 듯! */}
-          </a>
-          <Link to="/main">개발용 MAIN 창으로</Link>
-        </Grid>
+        <img src={mainLogo} alt="logo" width="90%" />
+        <img
+          src={
+            "https://aztipictures.s3.ap-northeast-2.amazonaws.com/azti_pic/" +
+            Object.keys(koreanAztiType)[sPick] +
+            ".png"
+          }
+          alt="logo"
+          width="50%"
+        />
+        <br />
+        <a href={KAKAO_AUTH_URL}>
+          <img src={kakaoBtn} alt="logo" width="50%" />
+        </a>
       </Grid>
-    </Box>
+    </>
   )
 }
